56440d935fe6a0f40a30baeb1d6c95c72535fd85
Tony <legendmastertony@gmail.com>
2023-05-17 11:16:50 +0800

    Fix require restart when Mail app updates - Show a message box on some errors

--- WindowsMailAutoSyncFix.cpp
+++ WindowsMailAutoSyncFix.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+
 #using <System.dll>
 #include <ShObjIdl.h>
 #include <appmodel.h>
@@ -8,9 +10,29 @@ const wchar_t* PACKAGE_FAMILY_NAME = L"microsoft.windowscommunicationsapps_8weky
 const wchar_t* APP_USER_MODEL_ID = L"microsoft.windowscommunicationsapps_8wekyb3d8bbwe!microsoft.windowslive.mail";
 const wchar_t* BACKGROUND_EXE_NAME = L"HxTsr.exe";
 
+const wchar_t* ERROR_MESSAGE_BOX_TITLE = L"Mail Auto Sync Fix";
+
 wchar_t background_exe_path[MAX_PATH];
 
-DWORD GetFirstPidByPath(PCWSTR path) {
+void ShowErrorMessageBox(long error_code) {
+	wchar_t error_text[1000];
+	if (!FormatMessage(
+		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK,
+		NULL,
+		error_code,
+		MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+		// 0,
+		error_text,
+		sizeof(error_text),
+		NULL))
+	{
+		MessageBox(NULL, L"Something wrong when trying to get the error message", ERROR_MESSAGE_BOX_TITLE, MB_ICONERROR);
+		return;
+	}
+	MessageBox(NULL, error_text, ERROR_MESSAGE_BOX_TITLE, MB_ICONERROR);
+}
+
+DWORD GetFirstPidByPath(wchar_t* path) {
 	DWORD processes[1024];
 	DWORD needed;
 	if (!EnumProcesses(processes, sizeof(processes), &needed)) {
@@ -19,8 +41,8 @@ DWORD GetFirstPidByPath(PCWSTR path) {
 
 	for (auto i = 0; i < needed / sizeof(DWORD); i++) {
 		auto pid = processes[i];
-		auto handle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid);
-		WCHAR file_name[MAX_PATH];
+		auto handle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, pid);
+		wchar_t file_name[MAX_PATH];
 		GetModuleFileNameEx(handle, NULL, file_name, MAX_PATH);
 		CloseHandle(handle);
 		if (wcscmp(file_name, path) == 0) {
@@ -37,79 +59,47 @@ void SystemEvents_PowerChanged(System::Object^ sender, Microsoft::Win32::PowerMo
 		if (pid == 0) {
 			return;
 		}
-		HANDLE handle = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+		HANDLE handle = OpenProcess(PROCESS_TERMINATE, false, pid);
 		TerminateProcess(handle, 0); // Not sure what error code should I use
 		CloseHandle(handle);
 	}
 }
 
-int main() {
-
-	// Allow single instance only
-	HRESULT hr;
-	WCHAR us_path[MAX_PATH];
-	hr = GetModuleFileName(NULL, us_path, MAX_PATH);
-	if (hr == 0) {
-		return hr;
-	}
-	// Replace '\' with '/' since CreateMutex treats it as a namespace separator
-	// https://devblogs.microsoft.com/oldnewthing/20120112-00/?p=8583
-	for (auto i = 0; i < MAX_PATH; i++) {
-		auto character = us_path[i];
-		if (character == L'\0') {
-			break;
-		}
-		if (character == L'\\') {
-			us_path[i] = L'/';
-		}
-	}
-	wchar_t mutex_name[MAX_PATH + 7] = L"Global\\";
-	wcsncat_s(mutex_name, us_path, MAX_PATH);
-	auto handle = CreateMutex(NULL, TRUE, mutex_name);
-	if (handle == NULL || GetLastError() == ERROR_ALREADY_EXISTS) {
-		return 1;
-	}
-
-	// System.dll seems to have called CoInitialize already
-	//auto hr = CoInitialize(NULL);
-	//if (hr != S_OK) {
-	//	return hr;
-	//}
-
+long MainFunctions() {
 	// Get package full name
-	UINT32 count = 0;
-	UINT32 buffer_length = 0;
-	hr = GetPackagesByPackageFamily(PACKAGE_FAMILY_NAME, &count, NULL, &buffer_length, NULL);
-	if (count < 1 || hr != ERROR_INSUFFICIENT_BUFFER) {
-		return hr;
+	uint32_t count = 0;
+	uint32_t buffer_length = 0;
+	auto error_code = GetPackagesByPackageFamily(PACKAGE_FAMILY_NAME, &count, NULL, &buffer_length, NULL);
+	if (count < 1 || error_code != ERROR_INSUFFICIENT_BUFFER) {
+		return error_code;
 	}
-	auto full_names = new PWSTR[count];
-	auto buffer = new WCHAR[buffer_length];
-	hr = GetPackagesByPackageFamily(PACKAGE_FAMILY_NAME, &count, full_names, &buffer_length, buffer);
-	if (hr != S_OK) {
-		return hr;
+	auto full_names = std::make_unique<wchar_t* []>(count);
+	auto buffer = std::make_unique<wchar_t[]>(buffer_length);
+	error_code = GetPackagesByPackageFamily(PACKAGE_FAMILY_NAME, &count, full_names.get(), &buffer_length, buffer.get());
+	if (error_code != NO_ERROR) {
+		return error_code;
 	}
 	auto package_full_name = full_names[0];
 
 	// Get background exe path (for suspend/resume on sleep/wakeup)
-	UINT32 path_length = 0;
-	hr = GetPackagePathByFullName(package_full_name, &path_length, NULL);
-	if (hr != ERROR_INSUFFICIENT_BUFFER) {
-		return hr;
+	uint32_t path_length = 0;
+	error_code = GetPackagePathByFullName(package_full_name, &path_length, NULL);
+	if (error_code != ERROR_INSUFFICIENT_BUFFER) {
+		return error_code;
 	}
-	auto package_path = new WCHAR[path_length];
-	hr = GetPackagePathByFullName(package_full_name, &path_length, package_path);
-	if (hr != S_OK) {
-		return hr;
+	auto package_path = std::make_unique<wchar_t[]>(path_length);
+	error_code = GetPackagePathByFullName(package_full_name, &path_length, package_path.get());
+	if (error_code != NO_ERROR) {
+		return error_code;
 	}
-	auto out = PathCombine(background_exe_path, package_path, BACKGROUND_EXE_NAME);
+	auto out = PathCombine(background_exe_path, package_path.get(), BACKGROUND_EXE_NAME);
 	if (out == NULL) {
 		return 1;
 	}
 
 	// Enable debug
 	IPackageDebugSettings* package_debug_settings;
-	hr = CoCreateInstance(
+	auto hr = CoCreateInstance(
 		CLSID_PackageDebugSettings,
 		NULL,
 		CLSCTX_ALL,
@@ -125,12 +115,55 @@ int main() {
 		return hr;
 	}
 
+	return NO_ERROR;
+}
+
+int main() {
+
+	// MessageBox(NULL, L"Text", ERROR_MESSAGE_BOX_TITLE, MB_ICONWARNING);
+
+	// Allow single instance only
+	wchar_t us_path[MAX_PATH];
+	auto path_length = GetModuleFileName(NULL, us_path, MAX_PATH);
+	if (path_length == 0) {
+		return GetLastError();
+	}
+	// Replace '\' with '/' since CreateMutex treats it as a namespace separator
+	// https://devblogs.microsoft.com/oldnewthing/20120112-00/?p=8583
+	for (auto i = 0; i < MAX_PATH; i++) {
+		auto character = us_path[i];
+		if (character == L'\0') {
+			break;
+		}
+		if (character == L'\\') {
+			us_path[i] = L'/';
+		}
+	}
+	wchar_t mutex_name[MAX_PATH + 7] = L"Global\\";
+	wcsncat_s(mutex_name, us_path, MAX_PATH);
+	auto handle = CreateMutex(NULL, true, mutex_name);
+	if (handle == NULL || GetLastError() == ERROR_ALREADY_EXISTS) {
+		return 1;
+	}
+
+	// System.dll seems to have called CoInitialize already
+	//auto hr = CoInitialize(NULL);
+	//if (hr != S_OK) {
+	//	return hr;
+	//}
+
+	auto error_code = MainFunctions();
+	if (error_code != NO_ERROR) {
+		ShowErrorMessageBox(error_code);
+		return error_code;
+	}
+
 	// Suspend and resume background exe on power state change
 	Microsoft::Win32::SystemEvents::PowerModeChanged += gcnew Microsoft::Win32::PowerModeChangedEventHandler(SystemEvents_PowerChanged);
 
 	// Prelaunch once terminated
 	IApplicationActivationManager* app_activation_manager;
-	hr = CoCreateInstance(
+	auto hr = CoCreateInstance(
 		CLSID_ApplicationActivationManager,
 		NULL,
 		CLSCTX_ALL,
@@ -141,13 +174,25 @@ int main() {
 		return hr;
 	}
 
+	auto failed_once = false;
 	while (true) {
 		DWORD pid;
-		hr = app_activation_manager->ActivateApplication(APP_USER_MODEL_ID, NULL, AO_PRELAUNCH, &pid);
+		hr = app_activation_manager->ActivateApplication(APP_USER_MODEL_ID, NULL, AO_PRELAUNCH | AO_NOERRORUI, &pid);
 		if (hr != S_OK) {
-			return hr;
+			failed_once = true;
+			Sleep(5000);
+			continue;
 		}
-		auto handle = OpenProcess(SYNCHRONIZE, FALSE, pid);
+		if (failed_once) {
+			failed_once = false;
+			error_code = MainFunctions();
+			if (error_code != NO_ERROR) {
+				ShowErrorMessageBox(error_code);
+				return error_code;
+			}
+			// MessageBox(NULL, L"Restart succeed", ERROR_MESSAGE_BOX_TITLE, MB_ICONINFORMATION);
+		}
+		auto handle = OpenProcess(SYNCHRONIZE, false, pid);
 		auto status = WaitForSingleObject(handle, INFINITE);
 		if (status != WAIT_OBJECT_0) {
 			Sleep(1000); // Prevent infinite loop consume too much cpu
